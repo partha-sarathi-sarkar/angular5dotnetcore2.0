7349274f634532c6f12b0d6aac60db4cb54b61f8
partha-sarathi-sarkar <53785505+partha-sarathi-sarkar@users.noreply.github.com>
2019-09-25 13:02:44 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -ev
-
-sudo apt install azure-cli=2.0.25.1
-az login -u partha.sarathi.sarkar95@outlook.com -p partha9007976323
-
-
 # Login to Docker Hub and upload images
 docker build --no-cache -t dotnetapp .
-docker tag dotnetapp:latest democontainercr.azurecr.io/dotnetapp:v1
-az acr login --name democontainercr
-docker push democontainercr.azurecr.io/dotnetapp:v1
+docker tag dotnetapp:latest spartha1995/automatedbuilddemo:latest
+docker push spartha1995/automatedbuilddemo:latest
