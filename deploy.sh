03202da75c3b17aa0ad250cb0f9aabee3bf2869f
partha-sarathi-sarkar <53785505+partha-sarathi-sarkar@users.noreply.github.com>
2019-09-25 18:36:49 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ev
 # Login to Docker Hub and upload images
-sudo docker build --no-cache -t dotnetapp .
-sudo docker tag dotnetapp:latest spartha1995/automatedbuilddemo:latest
-sudo docker push spartha1995/automatedbuilddemo:latest
+docker build --no-cache -t dotnetapp .
+docker tag dotnetapp:latest spartha1995/automatedbuilddemo:latest
+docker push spartha1995/automatedbuilddemo:latest
