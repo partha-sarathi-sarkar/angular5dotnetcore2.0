b4e1e2a52301d2ce2fc0455bb7064cea7f493625
Partha Sarkar <ptsarkar@cygnet.com>
2019-11-11 13:20:12 +0530

    Added files

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,3 @@
 call docker build -t automatedbuilddemo .
-call docker tag automatedbuilddemo spartha1995/automatedbuilddemo:${BUILD_NUMBER}
+call docker tag automatedbuilddemo spartha1995/automatedbuilddemo:%BUILD_NUMBER%
 call docker push spartha1995/automatedbuilddemo:%BUILD_NUMBER%
