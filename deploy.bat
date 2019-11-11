c87091a05f8ad77cf3a116da696c7bff7bf95ccc
Partha Sarkar <ptsarkar@cygnet.com>
2019-11-11 17:29:52 +0530

    Added files

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,4 @@
-%imageTag% = "V1.0.0.%BUILD_NUMBER%"
-echo %imageTag%
-
 call docker build -t automatedbuilddemo .
-call docker tag automatedbuilddemo spartha1995/automatedbuilddemo:%imageTag%
+call docker tag automatedbuilddemo spartha1995/automatedbuilddemo:%BUILD_NUMBER%
 call docker login -u spartha1995 --password-stdin
-call docker push spartha1995/automatedbuilddemo:%imageTag%
+call docker push spartha1995/automatedbuilddemo:%BUILD_NUMBER%
